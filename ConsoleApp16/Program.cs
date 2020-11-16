acbb0448bd96185f8589427b9723abe02960e820
Jonas Tibbling <jonas.tibbling@inet.se>
2020-11-16 09:50:09 +0100

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			await using var connection = new SqlConnection();
+
+			Console.WriteLine("Test");
+		}
+	}
+}
